d311f9b8ab4a1e2ef5b3a534f32228d98f46e037
Basashi12 <nyy2008@gmail.com>
2016-03-29 15:27:52 -0400

    Added files via upload

---
+++ simpletimer.py
@@ -0,0 +1,17 @@
+# Difficult challenge no. 2
+
+import time
+
+def main():
+    pass
+
+def timer():
+    t0 = time.time()
+    print('Timer has started.')
+    input('Press enter to continue: ')
+    t1 = time.time()
+    print(t1 - t0)
+
+
+if __name__ == '__main__':
+    main()   
