e873db244f9b41601736487a38143f2e641820ec
Basashi12 <nyy2008@gmail.com>
2016-03-30 01:22:41 -0400

    Added files via upload

--- simpletimer.py
+++ simpletimer.py
@@ -6,12 +6,15 @@ def main():
     pass
 
 def timer():
+    print('Timer will start when you press enter.')
+    input('Press enter to continue: ')
     t0 = time.time()
     print('Timer has started.')
+    print('Timer will stop when you press enter.')
     input('Press enter to continue: ')
+    print('Timer has stopped.')
     t1 = time.time()
     print(t1 - t0)
-
 
 if __name__ == '__main__':
     main()   
