8e86faf6e04131b5d12696cf94c3ff6e9049c52f
Basashi12 <nyy2008@gmail.com>
2016-03-30 11:37:21 -0400

    Added files via upload

--- calcpi.py
+++ calcpi.py
@@ -6,21 +6,21 @@ def main():
     pass
 
 def prec(n):
-    decimal.getcontext().prec = n+2
-    calc_pi(n)
-
+    # sets the decimal precision to at least n, using decimal module
+    decimal.getcontext().prec = n+5
+    
 def calc_pi(n):
     # calculating pi to an accuracy of n decimal places
     delta = math.pow(10,-n)
-    actualpi = 4 * (4 * (math.atan(1/2)) - (math.atan(1/239)))
-    arctan = 0
-    i = 1
-    while abs(actualpi - pie) < delta:
-        term = ((-1)**(i + 1)) / (2 * (i - 1) + 1)
+    actualpi = 4 * (4 * (math.atan(1/5)) - (math.atan(1/239)))
+    arctan = 0 # initializing variables
+    i = 0
+    pie = 0
+    while abs(actualpi - pie) > delta:
+        term = math.pow(-1, i) / (2 * (i - 1) + 1)
         arctan += term
         pie = 4 * arctan
         i += 1
-        return pie
     print('After {} iterations, computed pi to {}th decimal precision.'.format(i, n))
     print(pie)
         
