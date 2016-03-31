43f453ac28157ca5a1cada68f67648c3aa5d0838
Basashi12 <nyy2008@gmail.com>
2016-03-31 07:20:26 -0400

    Added files via upload

---
+++ 99.py
@@ -0,0 +1,8 @@
+def ninetynine():
+    for n in range(99,2,-1):
+        print('{} bottles of beer on the wall, {} bottles of beer.  Take one down, pass it around, {} bottles of beer on the wall.'.format(n, n, n-1))
+    print('2 bottles of beer on the wall, 2 bottles of beer.  Take one down, pass it around, 1 bottle of beer on the wall.')
+    print('1 bottle of beer on the wall, 1 bottle of beer.  Take one down, pass it around, no more bottles of beer on the wall.')
+
+
+
