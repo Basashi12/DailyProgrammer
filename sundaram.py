a75f23bd113a77dd791f7c728c36a3543a7d4b5b
Basashi12 <nyy2008@gmail.com>
2016-04-03 01:02:09 -0400

    Added files via upload

--- sundaram.py
+++ sundaram.py
@@ -10,21 +10,22 @@ def sundaram(n):
     primes = [2] # 2 is prime.  Sundaram finds only odd primes.
     fsieve = []
     ssieve = []
-    x = list(range(1, n+1))
-    for i in range(1, n+1):
-        for j in range(i, n+1):     # j from i to n+1
-            # compute all i + j + 2*i*j
-            z = (i + j + 2 * i * j)
-            if z <= n:
+    x = list(range(1, (n+2)//2))        # only need n/2 numbers for sieve
+    for i in range(1, (n+2)//2):
+        for j in range(i, (n+2)//2):    # j from i to n+1
+            z = (i + j + 2 * i * j)     # compute all i + j + 2*i*j
+            if z <= (n+2)//2:
                 fsieve.append(z)
     fsieve = list(set(fsieve))
     # fsieve * 2 + 1 not prime
     for y in fsieve:    
-        x.remove(y)
+        if y <= (n+2)//2:
+            x.remove(y)
     for num in x:
         if 2 * num + 1 <= n:
             primes.append(2 * num + 1)
-    print('Printing primes up to {}.'.format(n))
+    print("Using Sundaram's sieve to find prime number(s) up to {}.".format(n))
+    print('Found {} prime number(s).'.format(len(primes)))
     print(primes)
    
 if __name__ == '__main__':
