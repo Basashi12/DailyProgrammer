9776bce7d26798aeb567b109192606d0f841ef9d
Basashi12 <nyy2008@gmail.com>
2016-04-05 19:04:49 -0400

    Added files via upload

---
+++ binarizenum.py
@@ -0,0 +1,15 @@
+# Challenge 25 Intermediate
+# https://www.reddit.com/r/dailyprogrammer/comments/qxvu2/3152012_challenge_25_intermediate/
+# Accept CMD argument for base 10 number and return binary number
+
+import sys
+
+def main():
+    num = int(sys.argv[1])
+    bi(num)
+
+def bi(n):
+    print(str(bin(n))[2:])
+
+if __name__ == '__main__':
+    main()
