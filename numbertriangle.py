034f514641ad19e3668609ee34b29ad9a0af23ee
KT12 <nyy2008@gmail.com>
2016-04-10 21:08:53 -0400

    Added files via upload

--- numbertriangle.py
+++ numbertriangle.py
@@ -1,14 +1,20 @@
 # Challenge 35 Easy
 # https://www.reddit.com/r/dailyprogrammer/comments/rr4y2/432012_challenge_35_easy/
+
 import math
 
 
 def trinum(n):
-    z = (math.sqrt((8 * n) + 1) - 1 / 2)
-    if z is int:
+    if (math.sqrt((8 * n) + 1) - 1) % 2 == 0:
+        z = (math.sqrt((8 * n) + 1) - 1) / 2
+        print(z, n)
         return z, n
     else:
         trinum(n-1)
 
 def triconstruct(z, n):
-    
+    for i in range(z, 0, -1):
+        for j in range(n-i+1, n+1):
+            print(j, end=' ')
+        n -= i
+        print('\n')
