73bede01a9ffd821172048f01bfc91a5a4ab2ef7
KT12 <nyy2008@gmail.com>
2016-05-10 18:14:04 -0400

    Update SS2.py

--- SS2.py
+++ SS2.py
@@ -1,7 +1,10 @@
-#Challenge 33 Easy
+# Challenge 34 Easy
+# https://www.reddit.com/r/dailyprogrammer/comments/rmmn8/3312012_challenge_34_easy/
+# Input list of 3 numbers
+# Output sum square or two larger numbers
 
  def ss2(nlist):
+    # nlist is input of 3 numbers
     nlist.remove(min(nlist))
     x = sum(i**2 for i in nlist)
     return x
-    
