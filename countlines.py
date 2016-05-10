4798b8cbe58278ed4eeb9c661733835cf224d317
KT12 <nyy2008@gmail.com>
2016-05-10 18:18:21 -0400

    Update countlines.py

--- countlines.py
+++ countlines.py
@@ -1,9 +1,13 @@
 # Challenge #37 Easy
 # https://www.reddit.com/r/dailyprogrammer/comments/rzdwq/482012_challenge_37_easy/
+# input : a file as an argument
+# output: counts the total number of lines.
+# for bonus, also count the number of words in the file. 
 
 def count(filename):
-    count = 0
-    wordcount = 0
-    for line in open(filename).readlines(): count += 1
-    for words in open(filename).read().split(): wordcount += 1
-    return count, wordcount
+    # initialize var
+    lcount = 0
+    wcount = 0
+    for line in open(filename).readlines(): lcount += 1
+    for words in open(filename).read().split(): wount += 1
+    return lcount, wcount
