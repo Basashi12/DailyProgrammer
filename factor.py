a006fd2f16df40142e3052d66b5b90847a48743c
KT12 <nyy2008@gmail.com>
2016-05-14 00:45:48 -0400

    Update factor.py

--- factor.py
+++ factor.py
@@ -1,16 +1,17 @@
 # Challenge # 12 intermediate
+# Create a program which will factor a number
 
 import math
 
 def main():
     pass
 
- def factoring(num):
+def factoring(num):
     onum = num
     factors = []
     ceil = math.floor(num / 2)
     i = 2
-    while i in range(2, ceil+1):
+    for i in range(2, ceil+1):
         if num % i == 0:
             factors.append(i)
             num = (num/i)
