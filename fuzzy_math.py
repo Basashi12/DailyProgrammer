caac737e3cb59912312654dc7789003f18bafad5
KT12 <nyy2008@gmail.com>
2016-05-14 19:03:51 -0400

    Update fuzzy_math.py

--- fuzzy_math.py
+++ fuzzy_math.py
@@ -1,4 +1,7 @@
 # challenge 4 intermediate
+# https://www.reddit.com/r/dailyprogrammer/comments/pm6sq/2122012_challenge_4_intermediate/
+# create a calculator program that will take an input
+# following normal calculator input (5*5+4) and give an answer (29)
 
 def main():
     pass
