c2af84af596691342118b0343918b0b997ee2053
KT12 <nyy2008@gmail.com>
2016-05-16 01:15:52 -0400

    Update mcdonalds.py

--- mcdonalds.py
+++ mcdonalds.py
@@ -1,13 +1,15 @@
 # Challenge 23 Intermediate
 # https://www.reddit.com/r/dailyprogrammer/comments/qulnf/3132012_challenge_23_intermediate/
-# Find all McNugget Numbers
+# Find all McNugget Numbers - numbers which are the sum of McNugget combinations
 
 def nugget(x):
     mnn = []
     q = [6, 9, 20]
+    # try all numbers from 1 to x
     sample = list(range(1, x+1))
-    # try brute force calculation
+    # brute force calculation
     for i in sample:
+        # floor divide i by 6 with "buffer" of 2
         n = (i // 6) + 2
         for j in range(0,n):
             for k in range(0,n):
@@ -18,7 +20,9 @@ def nugget(x):
                             mnn.append(i)
                         else:
                             continue
+                    else:
+                        continue
     print('Printing list of all non-McNugget numbers')
     print(list((set(sample) - set(mnn))))
 
-# Simple multiplication faster in lin 16 than using sum, zip, lambda    
+# Simple multiplication faster in line 18 than using sum, zip, lambda
